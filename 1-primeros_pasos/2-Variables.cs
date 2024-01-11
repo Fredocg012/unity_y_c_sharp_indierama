268c7dca7723fc5ee745341e5299850d820e2fa8
Alfredo Correa González <fredo012@comunidad.unam.mx>
2024-01-11 04:03:11 -0600

    Variables básicas

---
+++ 1-primeros_pasos/2-Variables.cs
@@ -0,0 +1,20 @@
+// Creando variables
+
+// Variable entera
+// Declaración de variable
+int myFirstVar;
+// Inicialización de variable
+myFirstVar = 1000;
+
+// Variable de tipo flotante
+float myFloat = 10.5f;
+
+// Variable tipo String
+
+string myString = "Hola mundo!";
+Console.WriteLine(myString);
+
+// Variable tipo booleana
+bool myBool = true;
+
+Console.ReadKey();
