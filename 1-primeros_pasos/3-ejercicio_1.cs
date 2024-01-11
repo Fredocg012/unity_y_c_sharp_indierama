91d1a03fa9e8b0453a04c0663c5ec14d8867eb3b
Alfredo Correa González <fredo012@comunidad.unam.mx>
2024-01-11 04:52:35 -0600

    Ejercicio 1 y actualización de apuntes

---
+++ 1-primeros_pasos/3-ejercicio_1.cs
@@ -0,0 +1,26 @@
+/*
+ * Ejercicio
+ * Determinar el tipo de dato adecuado para almacenar:
+ * Un puntaje.
+ * El tiempo de duración de una animación.
+ * La cantidad de enemigos de un nivel.
+ * Si un jugador está vivo o muerto.
+ * El nombre de un item.
+ * 
+ * Declarar e inicializar dichas variables
+ */
+
+// Declarando variables e inicializando variables
+
+int score = 100;
+float run_animation_duration = 0.3f;
+int number_of_enemies = 30;
+bool player_is_alive = true;
+string item_1 = "Espada";
+
+Console.ReadKey();
+
+
+
+
+
